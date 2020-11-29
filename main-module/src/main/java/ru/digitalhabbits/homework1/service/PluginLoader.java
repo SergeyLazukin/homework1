83eee52b2eecd4bd0a2847ab0b1006df4daf96f1
SergeyLazukin <Lazykun@mail.ru>
2020-11-29 19:42:02 +0300

    Add loading plugins

--- main-module/src/main/java/ru/digitalhabbits/homework1/service/PluginLoader.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/PluginLoader.java
@@ -1,12 +1,18 @@
 package ru.digitalhabbits.homework1.service;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import ru.digitalhabbits.homework1.plugin.PluginInterface;
 
 import javax.annotation.Nonnull;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class PluginLoader {
@@ -18,6 +24,25 @@ public class PluginLoader {
     @Nonnull
     public List<Class<? extends PluginInterface>> loadPlugins(@Nonnull String pluginDirName) {
         // TODO: NotImplemented
-        return newArrayList();
+        String path = System.getProperty("user.dir") + "\\" + pluginDirName + "\\";
+
+        List<Class<? extends PluginInterface>> plugins = new ArrayList<>();
+
+        List<String> namePlugins = Arrays.asList("counter-plugin", "frequency-dictionary-plugin");
+        List<String> nameClassPlugins = Arrays.asList("CounterPlugin", "FrequencyDictionaryPlugin");
+
+        URL[] resources = new URL[namePlugins.size()];
+        try {
+            for (int i = 0; i < resources.length; i++) {
+                resources[i] = new File(path + namePlugins.get(i) + "." + PLUGIN_EXT).toURI().toURL();
+                URLClassLoader childClassloader = new URLClassLoader(new URL[]{resources[i]}, PluginLoader.class.getClassLoader());
+                Class<? extends PluginInterface> clazz = (Class<? extends PluginInterface>)
+                        Class.forName(PACKAGE_TO_SCAN + "." + nameClassPlugins.get(i), true, childClassloader);
+                plugins.add(clazz);
+            }
+        } catch (ClassNotFoundException | MalformedURLException e) {
+            logger.error(ExceptionUtils.getStackTrace(e));
+        }
+        return plugins;
     }
 }
