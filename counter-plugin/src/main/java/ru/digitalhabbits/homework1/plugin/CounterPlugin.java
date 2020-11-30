b8d861994d341bc23cc2b7366d3f129bf7e7797b
SergeyLazukin <Lazykun@mail.ru>
2020-11-30 09:06:00 +0300

    Counter plugin implementation

--- counter-plugin/src/main/java/ru/digitalhabbits/homework1/plugin/CounterPlugin.java
+++ counter-plugin/src/main/java/ru/digitalhabbits/homework1/plugin/CounterPlugin.java
@@ -2,6 +2,12 @@ package ru.digitalhabbits.homework1.plugin;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CounterPlugin
         implements PluginInterface {
@@ -10,6 +16,14 @@ public class CounterPlugin
     @Override
     public String apply(@Nonnull String text) {
         // TODO: NotImplemented
-        return "CounterPlugin: NotImplemented";
+//        String result = "";
+
+        int lines = text.split("\\n").length;
+        int words = text.split("\\s").length;
+        int letters = text.toCharArray().length;
+
+//        result = lines + ";" + words + ";" + letters;
+//        return result;
+        return lines + ";" + words + ";" + letters;
     }
 }
